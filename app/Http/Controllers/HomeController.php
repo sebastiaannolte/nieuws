c1c27a6516ece81f125093bb008f0f9cc37b9073
Sebastiaan <469879@student.saxion.nl>
2019-12-17 21:24:14 +0100

    l

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -83,8 +83,7 @@ class HomeController extends Controller
 
 
 
-        // $posts = $this->get_multi_result_set($this->tags)->paginate(5);
-        $posts = Posts::get()->paginate(5);
+        $posts = $this->get_multi_result_set($this->tags)->paginate(5);
         //$posts = Category::whereIn('id', $this->tags)->with('post_links')->get();
         // $posts = $posts->getRelationValue('');
         // foreach ($posts as $value) {
